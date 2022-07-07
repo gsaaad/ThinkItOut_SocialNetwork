aee90f767c0d10c45c3c72242d5056ac512972d3
gsaaad <67985748+gsaaad@users.noreply.github.com>
2022-07-07 19:50:09 -0400

    added dependencies, and got express app server working

---
+++ server.js
@@ -0,0 +1,18 @@
+// import express and mongoose DB
+
+const express = require("express");
+
+const app = express();
+const PORT = process.env.PORT || 3001;
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static("public"));
+
+app.get("/", (req, res) => {
+  res.send("Hello World!");
+});
+
+app.listen(PORT, () => {
+  console.log(`Connected on port ${PORT}`);
+});

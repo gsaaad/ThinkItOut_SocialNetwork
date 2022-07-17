9727f909f9f77f78869b1b904d277184b60aed14
gsaaad <67985748+gsaaad@users.noreply.github.com>
2022-07-17 14:41:03 -0400

    added user Model

--- controllers/user-controller.js
+++ controllers/user-controller.js
@@ -14,14 +14,62 @@ const userController = {
       });
   },
   getUserById({ params }, res) {
-    User.findOne({ _id: params.id }).then((dbUserData) => {
-      // if user id not found
-      if (!dbUserData) {
-        res
-          .status(404)
-          .json({ message: "No User found with this ID! Try again.." });
-      }
-      res.json(dbUserData);
-    });
+    User.findOne({ _id: params.id })
+      .then((dbUserData) => {
+        // if user id not found
+        if (!dbUserData) {
+          res
+            .status(404)
+            .json({ message: "No User found with this ID! Try again.." });
+        }
+        res.json(dbUserData);
+      })
+      .catch((err) => {
+        console.log(err);
+        res.status(400).json(err);
+      });
+  },
+  createUser({ body }, res) {
+    User.create(body)
+      .then((dbUserData) => res.json(dbUserData))
+      .catch((err) => {
+        console.log(err);
+        res.status(400).json(err);
+      });
+  },
+  updateUser({ params, body }, res) {
+    User.findOneAndUpdate({ _id: params.id }, body, { new: true })
+      .then((dbUserData) => {
+        // if no user found with this id
+        if (!dbUserData) {
+          res
+            .status(404)
+            .json({ message: "No user found with this ID, Try again" });
+          return;
+        }
+        res.json(dbUserData);
+      })
+      .catch((err) => {
+        res.status(400).json(err);
+      });
+  },
+  deleteUser({ params }, res) {
+    User.findOneAndDelete({ _id: params.id })
+      .then((dbUserData) => {
+        // if no user data found
+        if (!dbUserData) {
+          res
+            .status(404)
+            .json({ message: "There is not user with this id, try again" });
+          return;
+        }
+        res.json(dbUserData);
+      })
+      .catch((err) => {
+        console.log(err);
+        res.status(400).json(err);
+      });
   },
 };
+
+module.exports = userController;
